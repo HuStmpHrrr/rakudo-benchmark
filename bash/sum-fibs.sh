534ea20cc13a9f3b408bb453aef81305bf502d80
HuStmpHrrr <fdhzs2010@hotmail.com>
2015-06-05 22:13:45 -0400

    add sml perl5

--- bash/sum-fibs.sh
+++ bash/sum-fibs.sh
@@ -5,17 +5,19 @@ function sum_fibs() {
 	local i=0
 	while true; do
 		local r=`expr ${arr[$i]} + ${arr[$((i+1))]}`
-		if (( $r > 4000000 )); then
+		if (( "$r" > "4000000" )); then
 			break
 		fi
-		arr+=$r
+		arr+=($r)
 		((i++))
 	done
 	local v
-	local sum
+	local sum=0
 	for v in "${arr[@]}"; do
 		if (( $v % 2 == 0 )); then
 			((sum+=$v))
 		fi
 	done
 }
+
+{ time { for j in `seq 100`; do sum_fibs; done; }; } 2>&1 | tr 'ms' '  ' | awk '/real/ { print "sum fibs:", 60.0*$2+$3, "seconds / 100" }'
